759a6a8d80b3868d3980341e2fc43807fdae17dd
Yorick Demichelis <yorick@techholding.co>
2022-01-19 13:06:51 -0800

    feat: WIP

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"sort"
 
 	"github.com/google/go-github/v42/github"
 	"golang.org/x/oauth2"
@@ -17,16 +16,40 @@ var (
 	token = flag.String("token", "", "")
 )
 
+type ctxArgs string
+
 func CheckErr(err error) {
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
+func OrgCheck(ctx context.Context, client *github.Client) {
+	teams, _, err := client.Teams.ListTeams(ctx, *org, &github.ListOptions{})
+	CheckErr(err)
+
+	teamsJSON, err := json.Marshal(teams)
+	CheckErr(err)
+
+	fmt.Println(string(teamsJSON))
+}
+
+func OrgHandler(ctx context.Context, client *github.Client) {
+	args := flag.Args()
+	args = args[1:]
+
+	if len(args) > 0 && args[0] == "check" {
+		OrgCheck(ctx, client)
+	}
+}
+
 func main() {
 	flag.Parse()
+	args := flag.Args()
 
 	ctx := context.Background()
+	ctx = context.WithValue(ctx, ctxArgs("args"), args)
+
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: *token},
 	)
@@ -34,5 +57,10 @@ func main() {
 
 	client := github.NewClient(tc)
 
-	
+	// organization, _, err := client.Organizations.Get(ctx, *org)
+	// CheckErr(err)
+
+	if len(args) > 0 && args[0] == "org" {
+		OrgHandler(ctx, client)
+	}
 }
